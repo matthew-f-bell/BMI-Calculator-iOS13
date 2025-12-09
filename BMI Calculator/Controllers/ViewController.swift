271549d6d1f788ffba813cc724d55b3cced5e320
matthew-f-bell <xdjx3runx@gmail.com>
2025-12-09 08:49:06 -0800

    Lesson on multiple view controllers and how to connect them completed

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.2f m", sender.value)
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightLabel.text = String(format: "%.0f kg", sender.value)
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let bmi = weight / pow(height, 2)
+        print(bmi)
+        
+        let secondVC = SecondViewController()
+        secondVC.bmiValue = String(format: "%.1f", bmi)
+        
+        self.present(secondVC, animated: true, completion: nil)
+        
+    }
+    
 }
 
